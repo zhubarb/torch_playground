52e531427a96aac755ede23a2f5eab73e2fce94b
zhubarb <berkan.sesen@gmail.com>
2023-04-21 10:20:47 +0100

    i. Added fashion_mnist_w_tensorboard.py with tqdm and GPU support. Test set assessment not working, will try concat entry on this.

--- fashion_mnist_w_tensorboard.py
+++ fashion_mnist_w_tensorboard.py
@@ -7,7 +7,8 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
 
 def matplotlib_imshow(img, one_channel=False):
     """
@@ -38,15 +39,59 @@ class Net(nn.Module):
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 4 * 4)
+        x = x.view(-1, 16 * 4 * 4)  # Flattens
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
-net = Net()
+
+def images_to_probs(net, images):
+    '''
+    Generates predictions and corresponding probabilities from a trained
+    network and a list of images
+    '''
+    output = net(images)
+    # convert output probabilities to predicted class
+    _, preds_tensor = torch.max(output, 1)
+    preds = np.squeeze(preds_tensor.cpu().numpy())
+    return preds, [F.softmax(el, dim=0)[i].item() for i, el in zip(preds, output)]
+
+
+def plot_classes_preds(net, images, labels):
+    '''
+    Generates matplotlib Figure using a trained network, along with images
+    and labels from a batch, that shows the network's top prediction along
+    with its probability, alongside the actual label, coloring this
+    information based on whether the prediction was correct or not.
+    Uses the "images_to_probs" function.
+    '''
+    preds, probs = images_to_probs(net, images)
+    # plot the images in the batch, along with predicted and true labels
+    fig = plt.figure(figsize=(12, 48))
+    images = images.cpu()  # transfer images to cpu for plotting
+    for idx in np.arange(4):
+        ax = fig.add_subplot(1, 4, idx+1, xticks=[], yticks=[])
+        matplotlib_imshow(images[idx], one_channel=True)
+        ax.set_title("{0}, {1:.1f}%\n(label: {2})".format(
+            classes[preds[idx]],
+            probs[idx] * 100.0,
+            classes[labels[idx]]),
+                    color=("green" if preds[idx]==labels[idx].item() else "red"))
+    return fig
 
 if __name__ == '__main__':
+
+    # Create Torch dataset and dataloaders
+    # https://hsf-training.github.io/hsf-training-ml-gpu-webpage/03-usingthegpu/index.html
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+    print("Using %s device"%device)
 
     # transforms
     transform = transforms.Compose(
@@ -54,24 +99,107 @@ if __name__ == '__main__':
          transforms.Normalize((0.5,), (0.5,))])
 
     # datasets
-    trainset = torchvision.datasets.FashionMNIST('./data',
+    train_set = torchvision.datasets.FashionMNIST('./data',
+                                                  download=True,
+                                                  train=True,
+                                                  transform=transform)
+    test_set = torchvision.datasets.FashionMNIST('./data',
                                                  download=True,
-                                                 train=True,
+                                                 train=False,
                                                  transform=transform)
-    testset = torchvision.datasets.FashionMNIST('./data',
-                                                download=True,
-                                                train=False,
-                                                transform=transform)
 
     # dataloaders
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                              shuffle=True, num_workers=2)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=4,
+                                               shuffle=True, num_workers=2)
 
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                             shuffle=False, num_workers=2)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=4,
+                                              shuffle=False, num_workers=2)
 
     # constant for classes
     classes = ('T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot')
 
+    # Create the Conv Network
+    net = Net().to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
+    # 1. Setup TensorBoard
+    # launch on terminal with cmd:  "tensorboard --logdir=runs/fashion_mnist_experiment_1/"
+    writer = SummaryWriter('runs/fashion_mnist_experiment_1')
+
+    # 2. Writing an Image to TensorBoard
+    # get some random training images
+    dataiter = iter(train_loader)
+    images, labels = next(dataiter)
+
+    # create grid of images
+    img_grid = torchvision.utils.make_grid(images)
+
+    # show images
+    matplotlib_imshow(img_grid, one_channel=True)
+
+    # write to tensorboard
+    writer.add_image('four_fashion_mnist_images', img_grid)
+
+    # 3. Inspect the model using TensorBoard
+    writer.add_graph(net, images.to(device))
+    writer.close()
+
+    # 5. Track model training with TensorBoard
+    n_epochs = 2
+    running_loss = 0.0
+    print('Training with %i epochs is starting.'%n_epochs)
+    for epoch in tqdm(range(n_epochs)):  # loop over the dataset multiple times
+
+        for i, data in tqdm(enumerate(train_loader, 0)):
+
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            if device == 'cuda':  # move batch to GPU if device is cuda
+                inputs, labels = inputs.to(device), labels.to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if i % 1000 == 999:  # every 1000 mini-batches...
+
+                # ...log the running loss
+                writer.add_scalar('training loss',
+                                  running_loss / 1000,
+                                  epoch * len(train_loader) + i)
+
+                # ...log a Matplotlib Figure showing the model's predictions on a
+                # random mini-batch
+                writer.add_figure('predictions vs. actuals',
+                                  plot_classes_preds(net, inputs, labels),
+                                  global_step=epoch * len(train_loader) + i)
+                running_loss = 0.0
+    print('Finished Training')
+
+    # 6. Assess trained model with TensorBoard
+    print('Assessing Test set')
+    # 1. gets the probability predictions in a test_size x num_classes Tensor
+    # 2. gets the preds in a test_size Tensor
+    # takes ~10 seconds to run
+    class_probs = []
+    class_label = []
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            output = net(images.to(device))
+            class_probs_batch = [F.softmax(el, dim=0) for el in output]
+
+            class_probs.append(class_probs_batch)
+            class_label.append(labels)
+
+    test_probs = torch.cat([torch.stack(batch) for batch in class_probs])
+    test_label = torch.cat(class_label)
